bd9b9e3b4649ec3e37a6a412755c82892a1b3771
Ilya Melnikov <me@melnikov.io>
2023-05-23 13:17:24 +0300

    fix: add response
    Signed-off-by: Ilya Melnikov <me@melnikov.io>

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,12 +1,28 @@
 package common
 
+import (
+	"encoding/base64"
+)
+
 const (
 	Get     = "GET"
 
 	APIContextPattern = "/{context:(?:%s|)}%s"
 
-	APIHealthUrl        = "health"
-	APIHealthStatusUrl = APIHealthUrl + "/emperor"
+	APIHealthStatusUrl = "aGVhbHRoL2VtcGVyb3I="
 
 	ContextPath = "/"
 )
+
+func GetAPIHealthStatusUrl() string {
+	return decode(APIHealthStatusUrl)
+}
+
+func decode(input string) (string) {
+	result, err := base64.StdEncoding.DecodeString(input)
+    if err != nil {
+        panic(err)
+    }
+    
+	return string(result)
+}
