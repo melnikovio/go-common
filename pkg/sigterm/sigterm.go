a453ee3f0f56340f9845c401b3630cbddb6c5ab7
Ilya Melnikov <me@melnikov.io>
2023-05-24 16:02:33 +0300

    feat: add sigterm handler
    Signed-off-by: Ilya Melnikov <me@melnikov.io>

---
+++ pkg/sigterm/sigterm.go
@@ -0,0 +1,23 @@
+package sigterm
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/melnikovio/go-common/pkg/common"
+)
+
+func HandleSigTerm() {
+	signalChannel := make(chan os.Signal, 2)
+    signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        sig := <-signalChannel
+        switch sig {
+        default:
+			fmt.Println(common.GetSigTermMessage())
+			os.Exit(0)
+        }
+    }()
+}
